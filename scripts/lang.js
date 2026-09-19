660413ed68334fb0e203045fe46bda861a48cd8b
dimas70800 <dima.kucherenko7080@gmail.com>
2026-09-19 19:57:40 +0300

    Добавлено сглаживание в векторизацию

--- scripts/lang.js
+++ scripts/lang.js
@@ -317,6 +317,7 @@ const ru =
     vectorizeQuadsBlurLabel: "Толщина линий",
     vectorizeQuadsThreshLabel: "Порог чёрного",
     vectorizeQuadsDenoiseLabel: "Убрать одиночные пиксели",
+    vectorizeQuadsSmoothLabel: "Сглаживание",
     autoOptimizedResolution: "(авто-оптимизация разрешения)",
     fillRestoreBtn: "Предыдущая зона",
 
@@ -651,6 +652,7 @@ const en =
     vectorizeQuadsBlurLabel: "Line thickness",
     vectorizeQuadsThreshLabel: "Threshold of black",
     vectorizeQuadsDenoiseLabel: "Remove single pixels",
+    vectorizeQuadsSmoothLabel: "Smoothing",
     autoOptimizedResolution: "(auto-optimized resolution)",
     fillRestoreBtn: "Previous zone",
 
@@ -923,6 +925,7 @@ const langDependent =
         "vectorizeQuadsBlurLabel",
         "vectorizeQuadsThreshLabel",
         "vectorizeQuadsDenoiseLabel",
+        "vectorizeQuadsSmoothLabel",
         "fillRestoreBtn",
 
         "ctxDelete",
